67b6ddcc47cd165cc300c0276a6fac1caa4f05ac
HackFisher <hackfisher@gmail.com>
2018-08-30 01:17:19 +0800

    add interfaces and SettingIds

---
+++ contracts/SettingIds.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.24;
+
+/**
+    Id definitions for SettingsRegistry.sol
+    Can be used in conjunction with the settings registry to get properties
+*/
+contract SettingIds {
+    bytes32 public constant CONTRACT_RING_ERC20_TOKEN = "RINGERC20Token";
+
+    bytes32 public constant CONTRACT_ATLANTIS_ERC721LAND = "AtlantisERC721";
+
+    bytes32 public constant CONTRACT_CLOCK_AUCTION = "ClockAuction";
+
+    bytes32 public constant CONTRACT_LAND_DATA = "LandData";
+}
---
+++ contracts/interfaces/IBurnableERC20.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.4.23;
+
+interface IBurnableERC20 {
+    function burn(uint _value) public;
+}
