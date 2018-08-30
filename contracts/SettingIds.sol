f87bafca1c6e51e2e1f33c8d3d4110928ebbde8c
HackFisher <hackfisher@gmail.com>
2018-08-30 18:59:03 +0800

    add new settings for auction

--- contracts/SettingIds.sol
+++ contracts/SettingIds.sol
@@ -12,4 +12,20 @@ contract SettingIds {
     bytes32 public constant CONTRACT_CLOCK_AUCTION = "ClockAuction";
 
     bytes32 public constant CONTRACT_LAND_DATA = "LandData";
+
+    // Cut owner takes on each auction, measured in basis points (1/100 of a percent).
+    // Values 0-10,000 map to 0%-100%
+    // set ownerCut to 4%
+    // ownerCut = 400;
+    bytes32 public constant UINT_AUCTION_CUT = "UINT_AUCTION_CUT";  // Denominator is 10000
+
+
+    // default is 20 RING
+    // RING: 20000000000000000000
+    bytes32 public constant CONTRACT_AUCTION_CLAIM_BOUNTY = "CONTRACT_AUCTION_CLAIM_BOUNTY";  // Denominator is 10000
+
+    // BidWaitingTime in seconds, default is 30 minutes
+    // necessary period of time from invoking bid action to successfully taking the land asset.
+    // if someone else bid the same auction with higher price and within bidWaitingTime, your bid failed.
+    bytes32 public constant UINT_AUCTION_BID_WAITING_TIME = "UINT_AUCTION_BID_WAITING_TIME";
 }
--- contracts/interfaces/IBurnableERC20.sol
+++ contracts/interfaces/IBurnableERC20.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.4.23;
 
-interface IBurnableERC20 {
+contract IBurnableERC20 {
     function burn(uint _value) public;
 }
