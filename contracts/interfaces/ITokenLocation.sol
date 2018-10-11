28193f3370cc9fe25013ee8b3991323b8bef68e8
HackFisher <hackfisher@gmail.com>
2018-10-11 16:24:54 +0800

    rename to location

--- contracts/interfaces/ITokenLocation.sol
+++ contracts/interfaces/ITokenLocation.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.24;
 
-contract ITokenPosition {
+contract ITokenLocation {
 
     uint256 constant CLEAR_LOW = 0xffffffffffffffffffffffffffffffff00000000000000000000000000000000;
     uint256 constant CLEAR_HIGH = 0x00000000000000000000000000000000ffffffffffffffffffffffffffffffff;
@@ -9,26 +9,26 @@ contract ITokenPosition {
     // virtual api
     function getTokenLocation(uint _tokenId) public view returns (int, int);
 
-    function encodePositionId(int _x, int _y) public pure  returns (uint result) {
-        return _encodePositionId(_x, _y);
+    function encodeLocationId(int _x, int _y) public pure  returns (uint result) {
+        return _encodeLocationId(_x, _y);
     }
 
-    function _encodePositionId(int _x, int _y) internal pure  returns (uint result) {
-        return _unsafeEncodeTokenId(_x, _y);
+    function _encodeLocationId(int _x, int _y) internal pure  returns (uint result) {
+        return _unsafeEncodeLocationId(_x, _y);
     }
-    function _unsafeEncodeTokenId(int _x, int _y) internal pure  returns (uint) {
+    function _unsafeEncodeLocationId(int _x, int _y) internal pure  returns (uint) {
         return ((uint(_x) * FACTOR) & CLEAR_LOW) | (uint(_y) & CLEAR_HIGH) + 1;
     }
 
-    function decodePositionId(uint _positionId) public pure  returns (int, int) {
-        return _decodePositionId(_positionId);
+    function decodeLocationId(uint _positionId) public pure  returns (int, int) {
+        return _decodeLocationId(_positionId);
     }
-    function _decodePositionId(uint _positionId) internal pure  returns (int x, int y) {
-        (x, y) = _unsafeDecodePositionId(_positionId);
+    function _decodeLocationId(uint _positionId) internal pure  returns (int x, int y) {
+        (x, y) = _unsafeDecodeLocationId(_positionId);
     }
 
-    function _unsafeDecodePositionId(uint _value) internal pure  returns (int x, int y) {
-        require(_value > 0, "Position Id is start from 1, should larger than zero");
+    function _unsafeDecodeLocationId(uint _value) internal pure  returns (int x, int y) {
+        require(_value > 0, "Location Id is start from 1, should larger than zero");
         x = expandNegative128BitCast(((_value - 1) & CLEAR_LOW) >> 128);
         y = expandNegative128BitCast((_value - 1) & CLEAR_HIGH);
     }
