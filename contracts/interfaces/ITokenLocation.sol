f6488822dbb299acb0d967b0d63e9d18412f4886
HackFisher <hackfisher@gmail.com>
2018-10-16 00:12:43 +0800

    add support for locations with decimal 10**8, micron

--- contracts/interfaces/ITokenLocation.sol
+++ contracts/interfaces/ITokenLocation.sol
@@ -14,23 +14,49 @@ contract ITokenLocation {
     uint256 constant MAX_LOCATION_ID =    0x0100000000000000000000000000000000000000000000000000000000000000;
     uint256 constant FACTOR = 0x10000000000000000000000000000000; // <16 ** 31> or <2 ** 124>
 
-    // The location is in micron.
-    function getTokenLocation(uint256 _tokenId) public view returns (int, int);
+    int256 constant public HMETER_DECIMAL  = 10 ** 8;
+
+    // x, y should between -2^123 (-10633823966279326983230456482242756608) and 2^123 - 1 (10633823966279326983230456482242756607).
+    int256 constant public MIN_Location_XY = -10633823966279326983230456482242756608;
+    int256 constant public MAX_Location_XY = 10633823966279326983230456482242756607;
+    int256 constant public MAX_100M_DECIMAL  = 10633823966279326983230456482300000000;
+    int256 constant public MAX_100M  = 106338239662793269832304564823;
 
     function hasLocation(uint256 _tokenId) public view returns (bool);
 
+    // The location is in micron.
+
+    function getTokenLocation100M(uint256 _tokenId) public view returns (int, int){
+        (int _x, int _y) = getTokenLocation(_tokenId);
+        return ((_x + MAX_100M_DECIMAL)/HMETER_DECIMAL - MAX_100M, (_y + MAX_100M_DECIMAL)/HMETER_DECIMAL - MAX_100M);
+    }
+
+    function setTokenLocation100M(uint256 _tokenId, int _x, int _y) public{
+        setTokenLocation(_tokenId, _x * HMETER_DECIMAL, _y * HMETER_DECIMAL);
+    }
+
+    function getTokenLocation(uint256 _tokenId) public view returns (int, int);
+
     function setTokenLocation(uint256 _tokenId, int _x, int _y) public;
+
+    function encodeLocationId100M(int _x, int _y) public pure  returns (uint result) {
+        return encodeLocationId(_x * HMETER_DECIMAL, _y * HMETER_DECIMAL);
+    }
 
     function encodeLocationId(int _x, int _y) public pure  returns (uint result) {
         return _unsafeEncodeLocationId(_x, _y);
     }
 
-    // x, y should between -2^123 (-10633823966279326983230456482242756608) and 2^123 - 1 (10633823966279326983230456482242756607).
     function _unsafeEncodeLocationId(int _x, int _y) internal pure  returns (uint) {
-        require(_x >= -10633823966279326983230456482242756608 && _x <= 10633823966279326983230456482242756607, "Invalid value.");
-        require(_y >= -10633823966279326983230456482242756608 && _y <= 10633823966279326983230456482242756607, "Invalid value.");
-        
+        require(_x >= MIN_Location_XY && _x <= MAX_Location_XY, "Invalid value.");
+        require(_y >= MIN_Location_XY && _y <= MAX_Location_XY, "Invalid value.");
+
         return (((uint(_x) * FACTOR) & CLEAR_LOW) | (uint(_y) & CLEAR_HIGH)) + 1;
+    }
+
+    function decodeLocationId100M(uint _positionId) public pure  returns (int, int) {
+        (int _x, int _y) = decodeLocationId(_positionId);
+        return ((_x + MAX_100M_DECIMAL)/HMETER_DECIMAL - MAX_100M, (_y + MAX_100M_DECIMAL)/HMETER_DECIMAL - MAX_100M);
     }
 
     function decodeLocationId(uint _positionId) public pure  returns (int, int) {
--- test/TokenLocation.js
+++ test/TokenLocation.js
@@ -14,7 +14,7 @@ contract('Token Location Test', async(accounts) => {
         console.log('tokenLocation address: ', tokenLocation.address);
     })
 
-    it('test encode and decode', async() => {
+    it('test encode and decode in decimal', async() => {
 
         let locationId = await tokenLocation.encodeLocationId.call(-49 * HMETER, 49 * HMETER);
         let location = await tokenLocation.decodeLocationId.call(locationId);
@@ -24,6 +24,21 @@ contract('Token Location Test', async(accounts) => {
         let locationId1 = await tokenLocation.encodeLocationId.call(1, -1);
         console.log("locationId1... " + locationId1);
         let location1 = await tokenLocation.decodeLocationId.call(locationId1);
+        assert.equal(location1[0].toNumber(), 1);
+        assert.equal(location1[1].toNumber(), -1);
+
+    });
+
+    it('test encode and decode in 100M', async() => {
+
+        let locationId = await tokenLocation.encodeLocationId100M.call(-49, 49);
+        let location = await tokenLocation.decodeLocationId100M.call(locationId);
+        assert.equal(location[0].toNumber(), -49);
+        assert.equal(location[1].toNumber(), 49);
+
+        let locationId1 = await tokenLocation.encodeLocationId100M.call(1, -1);
+        console.log("locationId1... " + locationId1);
+        let location1 = await tokenLocation.decodeLocationId100M.call(locationId1);
         assert.equal(location1[0].toNumber(), 1);
         assert.equal(location1[1].toNumber(), -1);
 
