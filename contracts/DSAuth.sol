2f7ae234f81331961516f52f2f79ae06f1f325a8
HackFisher <hackfisher@gmail.com>
2018-10-19 19:19:45 +0800

    remove this from DSAuth for safty consideration

--- contracts/DSAuth.sol
+++ contracts/DSAuth.sol
@@ -1,18 +1,10 @@
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
 
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-// https://github.com/dapphub/ds-auth
-
+/**
+ * @title DSAuth
+ * @dev The DSAuth contract is reference implement of https://github.com/dapphub/ds-auth
+ * But in the isAuthorized method, the src from address(this) is remove for safty concern.
+ */
 pragma solidity ^0.4.23;
 
 import './interfaces/IAuthority.sol';
@@ -52,10 +44,13 @@ contract DSAuth is DSAuthEvents {
         _;
     }
 
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
     function isAuthorized(address src, bytes4 sig) internal view returns (bool) {
-        if (src == address(this)) {
-            return true;
-        } else if (src == owner) {
+        if (src == owner) {
             return true;
         } else if (authority == IAuthority(0)) {
             return false;
