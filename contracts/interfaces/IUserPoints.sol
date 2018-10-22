a8dabb8644ebd361ae247ebaf965a4470e00be38
HackFisher <hackfisher@gmail.com>
2018-10-22 22:52:07 +0800

    fix compile issue

--- contracts/interfaces/IUserPoints.sol
+++ contracts/interfaces/IUserPoints.sol
@@ -4,7 +4,7 @@ contract IUserPoints {
     event AddedPoints(address indexed user, uint256 pointAmount);
     event SubedPoints(address indexed user, uint256 pointAmount);
 
-    function addTickets(address _user, uint256 _pointAmount) public;
+    function addPoints(address _user, uint256 _pointAmount) public;
 
     function subPoints(address _user, uint256 _pointAmount) public;
 }
