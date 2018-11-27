c394ff8dbc02ab1fe1bbee1c50b758fdc3722aeb
董淑伟 <dsw0602@foxmail.com>
2018-11-27 17:53:10 +0800

    update IApostleBase.sol

--- contracts/interfaces/IApostleBase.sol
+++ contracts/interfaces/IApostleBase.sol
@@ -6,4 +6,6 @@ contract IApostleBase {
     function createApostle(uint256 _matronId, uint256 _sireId, uint256 _generation, uint256 _genes, uint256 _talents, address _owner) public;
 
     function isReadyToBreed(uint256 _apostleId) public view returns (bool);
+
+    function isAbleToBreed(uint256 _matronId, uint256 _sireId, address _owner) public view returns(bool);
 }
