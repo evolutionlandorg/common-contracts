b5554f2b32b68f2918bfd2d539f6bbd95f0c2963
HackFisher <hackfisher@gmail.com>
2018-12-04 15:17:00 +0800

    add view to strength of

--- contracts/interfaces/IMinerObject.sol
+++ contracts/interfaces/IMinerObject.sol
@@ -10,6 +10,6 @@ contract IMinerObject is ERC165  {
      *   bytes4(keccak256('strengthOf(uint256,address)'))
      */
 
-    function strengthOf(uint256 _tokenId, address _resourceToken) public returns (uint);
+    function strengthOf(uint256 _tokenId, address _resourceToken) public view returns (uint256);
 
 }
