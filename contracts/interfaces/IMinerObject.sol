6eb15c8e5c89c07e6ba6f0852015e54f77b06500
HackFisher <hackfisher@gmail.com>
2018-12-04 15:03:42 +0800

    update IMinerObject, query mining strength for specific resource

--- contracts/interfaces/IMinerObject.sol
+++ contracts/interfaces/IMinerObject.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.4.24;
 import "openzeppelin-solidity/contracts/introspection/ERC165.sol";
 
 contract IMinerObject is ERC165  {
-    bytes4 internal constant InterfaceId_IActivity = 0x5822a422;
+    bytes4 internal constant InterfaceId_IActivity = 0x64272b75;
     
     /*
-     * 0x5822a422 ===
-     *   bytes4(keccak256('strengthOf(uint256)'))
+     * 0x64272b752 ===
+     *   bytes4(keccak256('strengthOf(uint256,address)'))
      */
 
-    function strengthOf(uint256 _tokenId) public returns (uint);
+    function strengthOf(uint256 _tokenId, address _resourceToken) public returns (uint);
 
 }
