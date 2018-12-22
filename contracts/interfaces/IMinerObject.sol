cc124ae38ce50a0e75d43c8ca87e3a54350e6690
董淑伟 <dsw0602@foxmail.com>
2018-12-22 11:26:42 +0800

    refactor strengthOf with landTokenId in

--- contracts/interfaces/IMinerObject.sol
+++ contracts/interfaces/IMinerObject.sol
@@ -10,6 +10,6 @@ contract IMinerObject is ERC165  {
      *   bytes4(keccak256('strengthOf(uint256,address)'))
      */
 
-    function strengthOf(uint256 _tokenId, address _resourceToken) public view returns (uint256);
+    function strengthOf(uint256 _tokenId, address _resourceToken, uint256 _landTokenId) public view returns (uint256);
 
 }
