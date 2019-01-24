d70eb17c01e680dd279d6a5d6a5fb50465ec0e84
董淑伟 <dsw0602@foxmail.com>
2019-01-24 17:14:43 +0800

    modify ERC721 to be more general

--- contracts/ERC721Bridge.sol
+++ contracts/ERC721Bridge.sol
@@ -10,16 +10,10 @@ import "./interfaces/IMintableERC20.sol";
 
 contract ERC721Bridge is SettingIds, PausableDSAuth {
 
-    // type
-    struct LinkStatus {
-        uint256 linkTokenId;
-        bool isBridgedIn;
-    }
-
-// TODO: later
-//    struct WorkStatus {
-//
-//    }
+    // TODO: later
+    //    struct WorkStatus {
+    //
+    //    }
 
     /*
      *  Storage
@@ -33,10 +27,10 @@ contract ERC721Bridge is SettingIds, PausableDSAuth {
     uint128 public lastObjectId;
 
     // tokenId_outside_evolutionLand => tokenId_inside
-    mapping(uint256 => LinkStatus) public tokenIdOut2In;
+    mapping(uint256 => uint256) public tokenIdOut2In;
 
     // tokenId_inside => tokenId_outside
-    mapping(uint256 => LinkStatus) public tokenIdIn2Out;
+    mapping(uint256 => uint256) public tokenIdIn2Out;
 
 
 
@@ -64,51 +58,27 @@ contract ERC721Bridge is SettingIds, PausableDSAuth {
 
     function bridgeIn(uint256 _originTokenId) public {
         require(originNft.ownerOf(_originTokenId) == msg.sender, "Invalid owner!");
+        require(tokenIdOut2In[_originTokenId] == 0);
 
-        // not the first time to bridge in
-        if(tokenIdOut2In[_originTokenId].linkTokenId != 0) {
-            tokenIdOut2In[_originTokenId].isBridgedIn = true;
-            tokenIdIn2Out[tokenIdOut2In[_originTokenId].linkTokenId].isBridgedIn = true;
+        // first time to bridge in
+        lastObjectId += 1;
 
-        } else {
-            // first time to bridge in
-            lastObjectId += 1;
+        IInterstellarEncoderV3 interstellarEncoder = IInterstellarEncoderV3(registry.addressOf(SettingIds.CONTRACT_INTERSTELLAR_ENCODER));
+        address objectOwnership = registry.addressOf(SettingIds.CONTRACT_OBJECT_OWNERSHIP);
+        uint256 tokenId = interstellarEncoder.encodeTokenIdForOuterObjectContract(address(this), address(originNft), lastObjectId);
 
-            IInterstellarEncoderV3 interstellarEncoder = IInterstellarEncoderV3(registry.addressOf(SettingIds.CONTRACT_INTERSTELLAR_ENCODER));
-            address objectOwnership = registry.addressOf(SettingIds.CONTRACT_OBJECT_OWNERSHIP);
-            uint256 tokenId = interstellarEncoder.encodeTokenIdForOuterObjectContract(address(this), address(originNft), lastObjectId);
+        // link objects_in and objects_out
+        tokenIdOut2In[_originTokenId] = tokenId;
+        tokenIdIn2Out[tokenId] = _originTokenId;
 
-            // link objects_in and objects_out
-            tokenIdOut2In[_originTokenId].linkTokenId = tokenId;
-            tokenIdOut2In[_originTokenId].isBridgedIn = true;
-            tokenIdIn2Out[tokenId].linkTokenId = _originTokenId;
-            tokenIdIn2Out[tokenId].isBridgedIn = true;
-            // keep new mirror object in this contract
-            // before the owner has transferred his/her outerObject into this contract
-            // mirror object can not be transferred
-            IMintableERC20(objectOwnership).mint(address(this), tokenId);
-        }
+        // keep new mirror object in this contract
+        // before the owner has transferred his/her outerObject into this contract
+        // mirror object can not be transferred
+        IMintableERC20(objectOwnership).mint(address(this), tokenId);
 
 
         emit BridgeIn(_originTokenId, tokenId, address(originNft), msg.sender);
     }
 
-
-
-    function bridgeOut(uint256 _tokenId) public {
-        require(isValid(_tokenId), "already bridged out!");
-        uint256 originTokenId = tokenIdIn2Out[_tokenId].linkTokenId;
-        require(originNft.ownerOf(originTokenId) == msg.sender, "You have no right.");
-
-
-        tokenIdIn2Out[_tokenId].isBridgedIn = false;
-        tokenIdOut2In[originTokenId].isBridgedIn = false;
-    }
-
-
-    // this need to be checked every time
-    function isValid(uint256 _tokenId) public view returns (bool) {
-        return tokenIdIn2Out[_tokenId].isBridgedIn;
-    }
 
 }
