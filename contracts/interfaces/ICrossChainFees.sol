b2eeac8224888d5c634127adb84c744a572343c2
WoeOm <1015996366@qq.com>
2020-09-03 16:35:52 +0800

    remove issuing fee contract

--- contracts/interfaces/ICrossChainFees.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.4.24;
-
-contract ICrossChainFees {
-    function payTxFees(address _from, uint _value) public;
-}
--- migrations/12_crosschain_migration.js
+++ migrations/12_crosschain_migration.js
@@ -1,11 +1,9 @@
-const CrossChain = artifacts.require("CrossChain")
-const Proxy = artifacts.require('OwnedUpgradeabilityProxy')
-const CrossChainFees = artifacts.require("CrossChainFees")
+const Issuing = artifacts.require("Issuing")
 const ERC223 = artifacts.require("StandardERC223")
 const ISettingsRegistry = artifacts.require("ISettingsRegistry")
 
 module.exports = async (deployer, network, accounts) => {
-    console.log('CrossChain Test, deployer:', accounts, accounts[0])
+    console.log('Issuing Test, deployer:', accounts, accounts[0])
     if (network != "ropsten") {
         return
     }
@@ -14,56 +12,42 @@ module.exports = async (deployer, network, accounts) => {
         ropsten: {
             registry: "0x6982702995b053A21389219c1BFc0b188eB5a372",
             isPaused: false,
-            crossChainProxy: '0xff1338B614eD70FA15896673274e97679b32c11e',
-            crossChainFeesProxy: '0x67d2dEEB128C0C359919ed12E9cc2352DF531dfa',
             ring: '0xb52FBE2B925ab79a821b261C82c5Ba0814AAA5e0',
             kton: '0x1994100c58753793D52c6f457f189aa3ce9cEe94',
             settingsRegistry: '0x6982702995b053A21389219c1BFc0b188eB5a372'
         }
     }
 
-    let crossChainImpl = await CrossChain.new()
-    console.log('crossChain.address: ', crossChainImpl.address)
+    let issuing = await Issuing.new(params[network].settingsRegistry)
+    // let issuing = await Issuing.at('xxx')
+    console.log('issuing.address: ', issuing.address)
 
-    let crossChainProxy = await Proxy.at(params[network].crossChainProxy)
-    console.log('crossChainProxy deployed')
-
-    await crossChainProxy.upgradeTo(crossChain.address)
-    console.log('crossChain upgradeTo crossChainProxy')
-
-    let crossChainFeesImpl = await CrossChainFees.new();
-    console.log('crossChainFees.address: ', crossChainFeesImpl.address)
-
-    let crossChainFeesProxy = await Proxy.at(params[network].crossChainFeesProxy)
-    console.log('crossChainFeesProxy deployed')
-
-    await crossChainFeesProxy.upgradeTo(crossChainFeesImpl.address)
-    console.log('crossChainFees upgradeTo crossChainFeesProxy')
-
-    await crossChainFeesProxy.initializeContract(params[network].settingsRegistry, web3.utils.toWei('1'), false)
-
-    let crossChain = await CrossChain.at(params[network].crossChainProxy)
-    await crossChain.initializeContract(params[network].settingsRegistry, false)
-    await crossChain.addSupportToken(params[network].ring);
-    await crossChain.addSupportToken(params[network].kton);
-
+    // set ring authrity
     let registry = await ISettingsRegistry.at(params[network].settingsRegistry)
-    await registry.setAddressProperty('0x434f4e54524143545f43524f5353434841494e5f545846454553000000000000', params[network].crossChainFeesProxy )
 
-    let crossChainFees = await CrossChainFees.at(params[network].crossChainFeesProxy)
-    crossChainFees.addChannel(params[network].crossChainProxy)
+    //  UINT_ISSUING_FEE
+    await registry.setUintProperty('0x55494e545f49535355494e475f46454500000000000000000000000000000000', web3.utils.toWei('2'))
+
+    //  CONTRACT_ISSUING_POOL
+    await registry.setAddressProperty('0x434f4e54524143545f49535355494e475f504f4f4c0000000000000000000000', '0x7f5B598827359939606B3525712Fb124A1C7851d')
+    console.log('set registry success')
+
+    await issuing.addSupportedTokens(params[network].ring);
+    await issuing.addSupportedTokens(params[network].kton);
+    console.log('add supported tokens success')
 
     // test
     // crossChain test
     const RING = await ERC223.at(params[network].ring);
     const KTON = await ERC223.at(params[network].kton);
-    // // approve ring
-    await RING.approve(params[network].crossChainFeesProxy, web3.utils.toWei('10000'))
-    await KTON.approve(params[network].crossChainFeesProxy, web3.utils.toWei('10000'))
-    
-    let tx = await RING.transferFrom(accounts[0], params[network].crossChainProxy, web3.utils.toWei('1.2345'), '0xe44664996ab7b5d86c12e9d5ac3093f5b2efc9172cb7ce298cd6c3c51002c318')
+
+    // approve ring
+    await RING.approve(issuing.address, web3.utils.toWei('10000'))
+    console.log('approve ring success')
+
+    let tx = await RING.transferFrom(accounts[0], issuing.address, web3.utils.toWei('1.2345'), '0xe44664996ab7b5d86c12e9d5ac3093f5b2efc9172cb7ce298cd6c3c51002c318')
     console.log('transfer ring tx:', tx.tx)
 
-    tx = await KTON.transferFrom(accounts[0], params[network].crossChainProxy, web3.utils.toWei('0.0001234'), '0xe44664996ab7b5d86c12e9d5ac3093f5b2efc9172cb7ce298cd6c3c51002c318')
+    tx = await KTON.transferFrom(accounts[0], issuing.address, web3.utils.toWei('0.0001234'), '0xe44664996ab7b5d86c12e9d5ac3093f5b2efc9172cb7ce298cd6c3c51002c318')
     console.log('transfer kton tx:', tx.tx)
 }
