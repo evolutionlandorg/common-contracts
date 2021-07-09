d795de6d984a0c3131af57fb0f39a5a9b102dbea
echo <hujw77@gmail.com>
2021-07-09 10:21:06 +0800

    pet: polkadot adaptor auth

--- contracts/PolkaPetAdaptorAuthority.sol
+++ contracts/PolkaPetAdaptorAuthority.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.24;
 
-contract ERC721AdaptorAuthority {
+contract PolkaPetAdaptorAuthority {
     mapping (address => bool) public whiteList;
 
     constructor(address[] _whitelists) public {
